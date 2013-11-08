93d3ed26ef7c43cd4c4de71912b4e5185d63aef5
Nathan Woods <big.nate.w@gmail.com>
2013-11-07 23:21:24 -0700

    moving/adding things, just getting prepared

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,25 @@
 // Initial application
 
-var app = angular.module('myApp', []);
+angular.module('myApp', [
+	'ngRoute',
+	'myApp.controllers'
+]).
 
-app.controller('MainCtrl', function($scope) {
-	$scope.showAlert = function(q) {
-		alert(q);
-	}; 
-});
+config(['$routeProvider', function( $routeProvider ){
+	$routeProvider.
+		when('/cats', {templateUrl: 'partials/list-cat.tpl.html', controller: 'ListCatsCtrl'}).
+		when('/cats/:categoryID', {templateUrl: 'partials/list-item.tpl.html', controller: 'ListItemCtrl'}).
+		otherwise({redirectTo: '/cats'});
+}]);
+
+
+/*
+
+/ = Home - dynamically generated (static tpl with links)
+/Products = List Products - render list of offered products
+/Products/:prodID = List Items - render list of items within product
+/Products/:prodID/:itemID = Show Item details (add to cart)
+/Cart = View items in cart
+/Cart/checkout = form generated for checkout
+
+*/
