0152a9810e487edc0dcdc601b8834f5b1ac027bb
Nathan Woods <big.nate.w@gmail.com>
2013-11-10 22:50:44 -0700

    added cart and thinking about thumbnails

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -9,8 +9,10 @@ controller('IndexCtrl', ['$scope', 'myPage', function ($scope, myPage) {
 	myPage.setTitle("Upstream Academy", "Guiding accounting firms to high performance");
 }]).
 
-controller('ListProdCtrl', ['$scope', 'myPage', function ($scope, myPage) {
-	myPage.setTitle("Product Lines", "Some quote about products");
+controller('ListProdCtrl', ['$scope', 'myPage', 'prodList', 'myCart', function ($scope, myPage, prodList, myCart) {
+	myPage.setTitle("Our Products", "Some quote about products");
+
+	$scope.myCart = myCart;
 
 	$scope.sortField = undefined;
 	$scope.reverse = false;
@@ -35,40 +37,54 @@ controller('ListProdCtrl', ['$scope', 'myPage', function ($scope, myPage) {
 	$scope.pageNo = 1;
 	$scope.pageSize = 10;
 
-	$scope.products = [];
-	for (var i=0; i<45; i++) {
-		$scope.products.push({
-			productID: i,
-			name: 'Product ' + ("0" + i).slice(-2),
-			link: 'product-' + ("0" + i).slice(-2),
-			desc: 'Description of product ' + ("0" + i).slice(-2)
-		});
-	}
-
-	// $scope.showPage = ;
-
+	$scope.products = prodList;
 }]).
 
 controller('ListItemCtrl', ['$scope', 'myPage', '$routeParams', function ($scope, myPage, $routeParams) {
 	console.log($routeParams);
 	myPage.setTitle("Products", $routeParams.prodID);
+
+	$scope.sortField = undefined;
+	$scope.reverse = false;
+
+	$scope.sort = function (fieldName) {
+		if ($scope.sortField === fieldName) {
+			$scope.reverse = !$scope.reverse;
+		} else {
+			$scope.sortField = fieldName;
+			$scope.reverse = false;
+		}
+	};
+
+	$scope.sortIcon = function (fieldName) {
+		if ($scope.sortField === fieldName) {
+			return $scope.reverse ? 'glyphicon-chevron-down' : 'glyphicon-chevron-up' ;
+		} else {
+			return '';
+		}
+	};
+
+	$scope.pageNo = 1;
+	$scope.pageSize = 10;
 }]).
 
 controller('ShowItemCtrl', ['$scope', 'myPage', function ($scope, myPage) {
 	myPage.setTitle("Item YYY");
 }]).
 
-controller('CartCtrl', ['$scope', 'myPage', function ($scope, myPage) {
+controller('CartCtrl', ['$scope', 'myPage', 'myCart', function ($scope, myPage, myCart) {
 	myPage.setTitle("Shopping Cart");
+
+	$scope.myCart = myCart;
 }]).
 
 controller('CheckoutCtrl', ['$scope', 'myPage', function ($scope, myPage) {
 	myPage.setTitle("Checkout");
 }]).
 
-controller('HeadCtrl', ['$scope', 'myPage', 'breadcrumbs', function ($scope, myPage, breadcrumbs) {
+controller('HeadCtrl', ['$scope', 'myPage', 'breadcrumbs', 'myCart', function ($scope, myPage, breadcrumbs, myCart) {
 	$scope.myPage = myPage;
 	$scope.breadcrumbs = breadcrumbs;
-
+	$scope.myCart = myCart;
 
 }]);
