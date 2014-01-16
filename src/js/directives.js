16430f6d62aedb90fc0fed6eec1606ac851479d8
bign8-AZ <nwoods@azworld.com>
2014-01-16 16:06:49 -0700

    phone format directive

--- src/js/directives.js
+++ src/js/directives.js
@@ -2,7 +2,7 @@ angular.module('myApp.directives', [
 
 ]).
 
-directive('uaPassValidate', function() {
+directive('uaPassValidate', function() { // http://bit.ly/1cwp3h0
 	return {
 		restrict: 'A', // restrict to an attribute
 		require: 'ngModel', // must have ng-model attribute
@@ -34,8 +34,52 @@ directive('uaBlank', function() {
 		require: 'ngModel',
 		link: function(scope, elem, attr, ctrl) {
 			ctrl.$parsers.push(function(value) {
-				console.log(value);
 				return value ? value : undefined;
+			});
+		}
+	};
+}).
+
+directive('uaPhone', function() {
+	var formatPhone = function (tel) {
+		if (!tel) return undefined;
+		var value = tel.toString().trim().replace(/[^0-9]/g, ''), country, city, number;
+
+		switch (value.length) {
+			case 10: // +1PPP####### -> C (PPP) ###-####
+				country = 1;
+				city = value.slice(0, 3);
+				number = value.slice(3);
+				break;
+
+			case 11: // +CPPP####### -> CCC (PP) ###-####
+				country = value[0];
+				city = value.slice(1, 4);
+				number = value.slice(4);
+				break;
+
+			case 12: // +CCCPP####### -> CCC (PP) ###-####
+				country = value.slice(0, 3);
+				city = value.slice(3, 5);
+				number = value.slice(5);
+				break;
+
+			default:
+				return tel;
+		}
+		if (country == 1) country = "";
+		number = number.slice(0, 3) + '-' + number.slice(3);
+		return (country + " (" + city + ") " + number).trim();
+	};
+
+	return {
+		restrict: 'A',
+		require: '?ngModel',
+		link: function($scope, elem, attr, ctrl) {
+			ctrl.$parsers.unshift(function (tel) {
+				tel = formatPhone(tel);
+				elem.val(tel);
+				return tel;
 			});
 		}
 	};
