1b9462516e4df91bb689924f37be087b9682d27a
bign8-AZ <nwoods@azworld.com>
2014-01-16 15:09:34 -0700

    password validation directive

--- src/js/directives.js
+++ src/js/directives.js
@@ -1,1 +1,42 @@
-angular.module('myApp.directives', []);
+angular.module('myApp.directives', [
+
+]).
+
+directive('uaPassValidate', function() {
+	return {
+		restrict: 'A', // restrict to an attribute
+		require: 'ngModel', // must have ng-model attribute
+		link: function(scope, elem, attr, ctrl) {
+			ctrl.$parsers.push(function( value ) {
+				var lowerAlph = !!(value || 'a').match(/[a-z]/); // pass if empty
+				var upperAlph = !!(value || 'A').match(/[A-Z]/);
+				var hasNumber = !!(value || '0').match(/[0-9]/);
+				var metLength = !!(value || 'abcdef').match(/.{6,}/);
+				var specialCh = !!(value || '!').match(/[^a-zA-Z0-9]/);
+				var valid = lowerAlph && upperAlph && hasNumber && metLength && specialCh;
+
+				ctrl.$setValidity('lowerAlph', lowerAlph);
+				ctrl.$setValidity('upperAlph', upperAlph);
+				ctrl.$setValidity('hasNumber', hasNumber);
+				ctrl.$setValidity('metLength', metLength);
+				ctrl.$setValidity('specialCh', specialCh);
+				ctrl.$setValidity('passValid', valid);
+				
+				return (valid && value) ? value : undefined; // but don't return object
+			});
+		}
+	};
+}).
+
+directive('uaBlank', function() {
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		link: function(scope, elem, attr, ctrl) {
+			ctrl.$parsers.push(function(value) {
+				console.log(value);
+				return value ? value : undefined;
+			});
+		}
+	};
+});
