66b1c30a9b24139b2a37f23933d43de2000f5486
bign8-AZ <nwoods@azworld.com>
2014-01-20 14:24:33 -0700

    moving offsite cart scripts onsite

---
+++ src/assets/uacart.js
@@ -0,0 +1,67 @@
+var UACart = (function($){
+	var setHandle = function(ele, has) {
+		ele.unbind('click');
+		if (has) {
+			ele.addClass('added').click(function() {
+				alert('Item has already been added to your cart.');
+			});
+			if (ele.find('.added').length === 0) ele.append('<span class="added"> (Added to cart)</span>');
+		} else {
+			ele.removeClass('added').click(addTo).find('.added').remove();
+		}
+	};
+	var addTo = function(e) {
+		var target = $(e.target).unbind('click'), data = target.data();
+		data.action = 'add';
+
+		$.ajax({
+			type: 'GET',
+			dataType: 'json',
+			data: data,
+			url: 'http://payment.upstreamacademy.com/cb.php',
+			crossDomain: true,
+			xhrFields: { withCredentials: true },
+			success: function( json, status, xhr ) {
+				setHandle(target, true);
+			}
+		});
+		return false;
+	};
+	var checkCart = function() {
+		$.ajax({
+			type: 'GET',
+			dataType: 'json',
+			url: 'http://payment.upstreamacademy.com/cb.php',
+			data: {action:'get'},
+			crossDomain: true,
+			xhrFields: { withCredentials: true},
+			success: function( json, status, xhr ) {
+				$('a.cartAdd').each(function() {
+					var ele = $(this);
+					setHandle(ele, json.indexOf("" + ele.data('itemId')) >= 0);
+				});
+			}
+		});
+	};
+	var timer;
+	var obj = {
+		init: function(selector) {
+			console.log('Initializing UACart');
+			selector = selector || 'a.cartAdd';
+			$(selector).click( addTo );
+			checkCart();
+			timer = setInterval(function() {
+				checkCart();
+			}, 60*1000);
+		},
+		stop: function() {
+			console.log('Timer Stopped');
+			return clearInterval(timer);
+		}
+	};
+	return obj;
+})(jQuery);
+
+$(document).ready(function() {
+	UACart.init('a.cartAdd');
+});
