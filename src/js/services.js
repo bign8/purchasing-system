563d2bdd496542deb3bc04ae5636cd190382d926
bign8-AZ <nwoods@azworld.com>
2014-01-23 10:52:36 -0700

    Moved sub/title assign to routeProvider and myPage
    	- Removed myPage dependencies from controllers
    	- Exceptions
    		- RegisterConFormCtrl
    		- HeadCtrl
    		- UserFormCtrl
    	- added sub/title attributes to routeProvider
    	- Modified myPage

--- src/js/services.js
+++ src/js/services.js
@@ -2,14 +2,18 @@ angular.module('myApp.services', [
 
 ]).
 
-factory('myPage', ['$rootScope', '$sce', function( $rootScope, $sce ){
+factory('myPage', ['$rootScope', '$sce', '$route', function( $rootScope, $sce, $route ){
 	$rootScope.pageTitle = "Upstream Academy Shop"; // for actual title - https://coderwall.com/p/vcfo4q
 	var subTitle = "";
 
+	$rootScope.$on('$routeChangeSuccess', function() {
+		$rootScope.pageTitle = $route.current.title || $rootScope.pageTitle;
+		subTitle = $route.current.subTitle || '';
+	});
 	return {
 		setTitle: function( newTitle, newSubTitle ){
 			$rootScope.pageTitle = newTitle;
-			subTitle = newSubTitle;
+			subTitle = newSubTitle || '';
 		},
 		getHTML: function() {
 			return $sce.trustAsHtml($rootScope.pageTitle + ' <small>' + subTitle + '</small>');
