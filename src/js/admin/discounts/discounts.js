50f52454a23122cc5fb74c99e03f7e638f28d987
bign8 <big.nate.w@gmail.com>
2014-01-28 14:10:59 -0700

    Enable/Disable discounts now possible

--- src/js/admin/discounts/discounts.js
+++ src/js/admin/discounts/discounts.js
@@ -1,10 +1,10 @@
 angular.module('myApp.admin.discounts', ['myApp.services']).
 
-config(['$routeProvider', 'securityAuthorizationProvider', function($routeProvider, securityAuthorizationProvider) {
+config(['$routeProvider', 'securityAuthorizationProvider', function ($routeProvider, securityAuthorizationProvider) {
 	$routeProvider.when('/admin/discounts', {
 		title: 'Manage Discounts',
 		templateUrl: 'js/admin/discounts/discounts.tpl.html',
-		controller: 'AdminDiscountsCtrl',
+		controller: 'DiscountsCtrl',
 		resolve: {
 			user: securityAuthorizationProvider.requireAdminUser,
 			discounts: ['interface', function (interface) {
@@ -14,6 +14,18 @@ config(['$routeProvider', 'securityAuthorizationProvider', function($routeProvid
 	});
 }]).
 
-controller('AdminDiscountsCtrl', ['$scope', 'discounts', function($scope, discounts){
+controller('DiscountsCtrl', ['$scope', 'discounts', 'interface', function ($scope, discounts, interface){
 	$scope.discounts = discounts;
+	$scope.clickCatch = function($event) { $event.stopPropagation(); };
+
+	$scope.edit = function(discount) {
+		console.log('editing');
+		console.log(discount);
+	};
+
+	$scope.change = function(discount) {
+		interface.admin('setDiscountActive', discount).then(function(res) {
+			discount = res;
+		});
+	};
 }]);
--- src/php/libinc/admin.php
+++ src/php/libinc/admin.php
@@ -13,14 +13,27 @@ class Admin extends NG {
 	public static function process( $action, &$pass, &$data ) {
 		$obj = new Admin();
 		switch ( $action ) {
-			case 'getDiscounts': $data = $obj->getDiscounts(); break;
+			case 'getDiscounts':      $data = $obj->getDiscounts();      break;
+			case 'setDiscountActive': $data = $obj->setDiscountActive(); break;
 			default: $pass = false;
 		}
 	}
+
+	// ----
+	// Discount Funcitons
+	// ----
 
 	// Worker(disocunts): returns all discounts
 	public function getDiscounts() {
 		$STH = $this->db->query("SELECT * FROM `discount`;");
 		return $STH->fetchAll( PDO::FETCH_ASSOC );
+	}
+
+	// Worker(discounts): assigns discont activness
+	public function setDiscountActive() {
+		$data = $this->getPostData();
+		$STH = $this->db->prepare("UPDATE `discount` SET `active`=? WHERE `discountID`=?;");
+		if (!$STH->execute($data->active, $data->discountID)) return $this->conflict();
+		return $data;
 	}
 }
