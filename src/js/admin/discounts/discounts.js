4027e2aa4d9d4562b76e40d21af3d2b849b58922
bign8-AZ <nwoods@azworld.com>
2014-01-29 10:15:48 -0700

    Adding discount service

--- src/js/admin/discounts/discounts.js
+++ src/js/admin/discounts/discounts.js
@@ -10,8 +10,8 @@ config(['$routeProvider', 'securityAuthorizationProvider', function ($routeProvi
 		controller: 'DiscountListCtrl',
 		resolve: {
 			user: securityAuthorizationProvider.requireAdminUser,
-			discounts: ['interface', function (interface) {
-				return interface.admin('getDiscounts');
+			discounts: ['DiscountService', function (DiscountService) {
+				return DiscountService.initList();
 			}]
 		}
 	}).when('/admin/discounts/:discountID', {
@@ -20,30 +20,23 @@ config(['$routeProvider', 'securityAuthorizationProvider', function ($routeProvi
 		controller: 'DiscountEditCtrl',
 		resolve: {
 			user: securityAuthorizationProvider.requireAdminUser,
-			discount: ['interface', function (interface) {
-				return interface.admin('getDiscount');
+			discount: ['DiscountService', '$route', function (DiscountService, $route) {
+				return DiscountService.getDiscount($route.current.params.discountID);
 			}]
 		}
 	});
 }]).
 
-controller('DiscountListCtrl', ['$scope', 'discounts', 'interface', '$modal', '$location', function ($scope, discounts, interface, $modal, $location){
+controller('DiscountListCtrl', ['$scope', 'discounts', '$modal', '$location', 'DiscountService', function ($scope, discounts, $modal, $location, DiscountService){
 	$scope.discounts = discounts;
-	$scope.clickCatch = function($event) { $event.stopPropagation(); };
-
-	$scope.edit = function(discount) {
-		console.log('editing');
-		console.log(discount);
-		$location.path('/admin/discounts/' + discount.discountID);
-	};
-
-	$scope.change = function(discount) {
-		interface.admin('setDiscountActive', discount).then(function(res) {
+	$scope.clickCatch = function ($event) { $event.stopPropagation(); };
+	$scope.edit = function (discount) { $location.path('/admin/discounts/' + discount.discountID); };
+	$scope.change = function (discount) {
+		DiscountService.setActive(discount).then(function(res) {
 			discount = res;
 		});
 	};
-
-	$scope.rem = function($event, discount) {
+	$scope.rem = function ($event, discount) {
 		$event.stopPropagation();
 		var modalInstance = $modal.open({
 			templateUrl: 'discountConfirmDelete.tpl.html',
@@ -56,14 +49,45 @@ controller('DiscountListCtrl', ['$scope', 'discounts', 'interface', '$modal', '$
 				discount: function() { return discount; }
 			}
 		});
-		modalInstance.result.then(function() {
-			interface.admin('remDiscount', discount).then(function() {
+		modalInstance.result.then(function () {
+			DiscountService.rem(discount).then(function () {
 				$scope.discounts.splice($scope.discounts.indexOf(discount), 1);
 			});
 		});
 	};
 }]).
 
-controller('DiscountEditCtrl', ['$scope', 'discount', 'interface', '$modal', function ($scope, discount, interface, $modal) {
+controller('DiscountEditCtrl', ['$scope', 'discount', 'DiscountService', function ($scope, discount, DiscountService) {
 	$scope.discount = discount;
+}]).
+
+factory('DiscountService', ['interface', function (interface) {
+	var casheDiscounts = {};
+	var service = {
+		initList: function () {
+			return interface.admin('getDiscounts').then(function (discounts) {
+				angular.forEach(discounts, function(discount) {
+					casheDiscounts[discount.discountID] = discount;
+				});
+				return discounts;
+			});
+		},
+		getDiscount: function (discountID) {
+			return casheDiscounts[discountID] || interface.admin('getDiscount', {discountID: discountID}).then(function (res) {
+				casheDiscounts[res.discountID] = res;
+				return res;
+			});
+		},
+		setActive: function (discount) {
+			return interface.admin('setDiscountActive', discount).then(function (res) {
+				casheDiscounts[res.discountID] = res;
+				return res;
+			});
+		},
+		rem: function (discount) {
+			return interface.admin('remDiscount', discount);
+		},
+
+	};
+	return service;
 }]);
