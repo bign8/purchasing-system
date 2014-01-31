1f374dc23ba21fec29ec01185bd48ff9688e6beb
bign8-AZ <nwoods@azworld.com>
2014-01-31 10:24:47 -0700

    Moving recipt-print

--- src/js/main/index.js
+++ src/js/main/index.js
@@ -1,4 +1,5 @@
 angular.module('myApp.main', [
 	'myApp.main.reset',
-	'myApp.main.payment'
+	'myApp.main.payment',
+	'myApp.main.recipt',
 ]);
---
+++ src/js/main/recipt/recipt.js
@@ -0,0 +1,19 @@
+angular.module('myApp.main.recipt', [
+	'security'
+]).
+
+config(['$routeProvider', 'securityAuthorizationProvider', function( $routeProvider, securityAuthorizationProvider ){
+	$routeProvider.when('/recipt', {
+		title: "Recipt",
+		subTitle: "from last purchase",
+		templateUrl: 'js/main/recipt/recipt.tpl.html',
+		controller: 'ReciptCtrl',
+		resolve: {
+			user: securityAuthorizationProvider.requireAuthenticatedUser
+		}
+	});
+}]).
+
+controller('ReciptCtrl', ['$scope', function ($scope) {
+	$scope.recipt = JSON.parse(localStorage.getItem('UA-recipt'));
+}]);
