fa2807d7ca5677ffc9d51116c4ad6cdc7e778610
bign8 <big.nate.w@gmail.com>
2014-01-31 10:55:12 -0700

    Moving list-purchases

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -258,28 +258,6 @@ controller('HeadCtrl', ['$scope', 'myPage', 'breadcrumbs', 'theCart', 'security'
 	$scope.security = security;
 }]).
 
-controller('ListPurchasesCtrl', ['$scope', 'items', '$modal', function ($scope, items, $modal){
-	$scope.items = items;
-	angular.forEach($scope.items, function(ele) {
-		if (ele.stamp) ele.stamp = new Date( ele.stamp );
-	});
-	$scope.showAttendees = function( item ) {
-		$modal.open({
-			templateUrl: 'partials/modal-list-attendees.tpl.html',
-			controller: 'ModalListAttendeesCtrl',
-			resolve: {
-				item: function() { return item; }
-			}
-		});
-	};
-}]).
-
-controller('ModalListAttendeesCtrl', ['$scope', '$modalInstance', 'item', function ($scope, $modalInstance, item) {
-	$scope.item = item;
-	$scope.ok = function () { $modalInstance.close('all good'); };
-	$scope.cancel = function () { $modalInstance.dismiss('cancel'); };
-}]).
-
 controller('RegisterFormCtrl', ['$scope', '$modal', 'interface', 'security', 'firms', 'appStrings', function ($scope, $modal, interface, security, firms, appStrings){
 
 	// find firm vs. register
--- src/js/main/index.js
+++ src/js/main/index.js
@@ -2,4 +2,5 @@ angular.module('myApp.main', [
 	'myApp.main.reset',
 	'myApp.main.payment',
 	'myApp.main.recipt',
+	'myApp.main.purchases',
 ]);
