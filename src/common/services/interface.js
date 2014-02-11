00fd5ff8c7f31838bf4b3f3ced1d358b0b4f2da4
bign8-AZ <nwoods@azworld.com>
2014-02-11 10:55:10 -0700

    local-dev: allowing local access to remote php

--- src/common/services/interface.js
+++ src/common/services/interface.js
@@ -26,7 +26,9 @@ factory('interface', ['$http', '$q', '$rootScope', '$timeout', function ($http, 
 		var deferred = $q.defer();
 		pendingPromisses++;
 
-		$http.post('/interface.php', myData, {params:{c:myClass, a:myFn}}).then(function(obj) {
+		var root = ( document.location.origin.indexOf('payment') === -1 ) ? 'http://payment.upstreamacademy.com/dev' : '' ; // Development
+
+		$http.post(root + '/interface.php', myData, {params:{c:myClass, a:myFn}}).then(function(obj) {
 			deferred.resolve( formatData( obj.data ) );
 			pendingPromisses--;
 		}, function(obj) {
