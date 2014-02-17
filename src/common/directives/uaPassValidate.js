443e48240a99316a9a432c0a6b5167b71943d4bf
bign8-AZ <nwoods@azworld.com>
2014-02-17 11:46:42 -0700

    release-0.2.00: losen pw requirements

--- src/common/directives/uaPassValidate.js
+++ src/common/directives/uaPassValidate.js
@@ -6,18 +6,13 @@ directive('uaPassValidate', function() { // http://bit.ly/1cwp3h0
 		require: 'ngModel', // must have ng-model attribute
 		link: function(scope, elem, attr, ctrl) {
 			ctrl.$parsers.push(function( value ) {
-				var lowerAlph = !!(value || 'a').match(/[a-z]/); // pass if empty
-				var upperAlph = !!(value || 'A').match(/[A-Z]/);
 				var hasNumber = !!(value || '0').match(/[0-9]/);
 				var metLength = !!(value || 'abcdef').match(/.{6,}/);
 				var specialCh = !!(value || '!').match(/[^a-zA-Z0-9]/);
-				var valid = lowerAlph && upperAlph && hasNumber && metLength && specialCh;
+				var valid = metLength && (hasNumber || specialCh);
 
-				ctrl.$setValidity('lowerAlph', lowerAlph);
-				ctrl.$setValidity('upperAlph', upperAlph);
-				ctrl.$setValidity('hasNumber', hasNumber);
 				ctrl.$setValidity('metLength', metLength);
-				ctrl.$setValidity('specialCh', specialCh);
+				ctrl.$setValidity('isSpecial', hasNumber || specialCh);
 				ctrl.$setValidity('passValid', valid);
 				
 				return (valid && value) ? value : undefined; // but don't return object
