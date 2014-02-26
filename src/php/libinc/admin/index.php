426ff6f897a407b97a571ffa81298fdaade6337d
bign8 <big.nate.w@gmail.com>
2014-02-26 15:25:43 -0700

    admin: remove admin switching

--- src/php/libinc/admin/index.php
+++ src/php/libinc/admin/index.php
@@ -3,11 +3,16 @@
 class Admin {
 	public static function process( $action, &$pass, &$data ) {
 		list( $area, $action ) = explode( '-', $action );
+		$area = strtolower( $area );
 
-		switch ( $area ) {
-			case 'discount': require_once( __DIR__ . '/discount.php' ); Discount::process( $action, $pass, $data ); break;
+		$admins = array(
+			'discount',
+		);
 
-			default: $pass = false;
-		}
+		if (in_array($area, $admins)) {
+			require_once( __DIR__ . '/' . $area . '.php');
+			$area = ucfirst($area);
+			$area::process( $action, $pass, $data );
+		} else $pass = false;
 	}
 }
