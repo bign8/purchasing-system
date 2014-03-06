09c2f4e06a8f94771c0d1bd2f05c78bb5a90c88f
bign8 <big.nate.w@gmail.com>
2014-03-06 10:31:14 -0700

    0.3.01: adding footer to all emails sent

--- src/php/libinc/user.php
+++ src/php/libinc/user.php
@@ -204,26 +204,6 @@ HTML;
 			<p>
 				If you have any questions or concerns, please feel free to contact me.
 			</p>
-			<p style='font-family:"Palatino Linotype","serif";font-variant:small-caps;'>
-				<span style='color:black;letter-spacing:1.1pt'>
-					<b style='color:navy'>Georgia Cummings</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					Director&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
-					<b>Upstream Academy</b>
-				</span>
-				<br/>
-				<span style='font-size:10pt;color:gray;letter-spacing:.4pt'>
-					P. O. Box 1147&nbsp;&nbsp;828 Great Northern Blvd&nbsp;&nbsp;Helena, MT 59624-1147
-					<br/>
-					(P) 406-495-1850&nbsp;&nbsp;&nbsp;(F) 406-442-1100&nbsp;&nbsp;&nbsp;
-					<a href="mailto:georgiac@upstreamacademy.com" style='color:gray;'>georgiac@upstreamacademy.com</a>
-				</span>
-				<br/>
-				<span style='font-size:10.0pt;color:navy;color:navy;letter-spacing:1.1pt;text-align:justify'>
-					Guiding accounting firms to high performance
-					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<a href="http://www.upstreamacademy.com/" style='color:#31849B;letter-spacing:1.0pt'>upstreamacademy.com</a>
-				</span>
-			</p>
 HTML;
 		$mail = new UAMail();
 		if (!$mail->sendMsg('Password Reset Instructions (payment.upstreamacademy.com)', $html, $user['email'], $user['legalName'])) $this->conflict('mail');
