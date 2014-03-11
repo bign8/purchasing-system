f4f682cd3992a3eafb8f2dbaf38f2ddacc3a4d16
bign8-AZ <nwoods@azworld.com>
2014-03-11 09:05:10 -0600

    firm-merge: finishing functionality

--- src/php/libinc/admin/merge.php
+++ src/php/libinc/admin/merge.php
@@ -25,14 +25,29 @@ class Merge extends NG {
 	// stores discount changes
 	public function set() {
 		$d = $this->getPostData();
-		if (isset($d->firmID)) {
-			$STH = $this->db->prepare("UPDATE firm SET name=?,website=? WHERE firmID=?;");
-			if (!$STH->execute($d->name, $d->website, $d->firmID)) return $this->conflict();
-		} else {
-			$STH = $this->db->prepare("INSERT INTO firm (name, website, addressID) VALUES (?,?,?);");
-			if (!$STH->execute($d->name, $d->website, $d->addr->addressID)) return $this->conflict();
-			$d = (object) array_merge( (array)$d, array('firmID' => $this->db->lastInsertId()) );
-		}
-		return $d;
+
+		// Purchases
+		$purchaseSTH = $this->db->prepare("UPDATE purchase SET firmID=? WHERE firmID=?;");
+		if (!$purchaseSTH->execute($d->destID, $d->srcID)) return $this->conflict();
+
+		// Contact
+		$contactSTH = $this->db->prepare("UPDATE contact SET firmID=? WHERE firmID=?;");
+		if (!$contactSTH->execute($d->destID, $d->srcID)) return $this->conflict();
+
+		// Set Address
+		$addressSTH = $this->db->prepare("INSERT INTO address (addrName,addr1,addr2,city,state,zip) VALUES (?,?,?,?,?,?);");
+		if (!$addressSTH->execute(
+			$d->merge->addrName, $d->merge->addr1, $d->merge->addr2, $d->merge->city, $d->merge->state, $d->merge->zip
+		)) return $this->conflict();
+
+		// Set Firm
+		$firmSTH = $this->db->prepare("UPDATE firm SET addressID=?, name=?, website=? WHERE firmID=?;");
+		if (!$firmSTH->execute( $this->db->lastInsertId(), $d->merge->name, $d->merge->website, $d->destID )) return $this->conflict();
+
+		// Remove Firm
+		$rmSTH = $this->db->prepare("DELETE FROM firm WHERE firmID=?;");
+		if (!$rmSTH->execute( $d->srcID )) return $this->conflict();
+		
+		return $d->merge;
 	}
 }
