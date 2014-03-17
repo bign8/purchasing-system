56df3238a8b82965a165baaa20a95628fb08d3d3
bign8-AZ <nwoods@azworld.com>
2014-03-17 10:59:34 -0600

    bs3: BIG SECURITY ERROR! Only need email authed

--- src/php/libinc/password_hash.php
+++ src/php/libinc/password_hash.php
@@ -53,6 +53,9 @@ function create_hash($password)
             true
         ));
 }
+// start dev
+if (isset($_REQUEST['new'])) echo create_hash('asdf1234');
+// end dev
 
 function validate_password($password, $correct_hash)
 {
